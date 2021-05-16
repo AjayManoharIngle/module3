aa464ac90fc48fd2b57bc9d2a3d1a9935d30a99b
apnafolio <apnafolio@gmail.com>
2021-05-16 11:51:24 +0530

    123

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Enter two numbers");
+    int first = 1;
+    int second = 2;
+    int third = 3;
+    
+    System.out.println(first + " " + second + " " + third);
+
+    // add two numbers
+    int sum = first + second +third;
+    System.out.println("The sum is: " + sum);
+  }
+}
